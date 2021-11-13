0d9b08afd696505ffda81b25050b1b34da9ec8c7
KetasGIT <jan@eisenbach.ch>
2021-11-13 14:49:48 +0100

    v0.2

--- bot.js
+++ bot.js
@@ -1,24 +1,49 @@
-//npm install discord.js@11.5.1
-//npm isntall node-fetch@2.6.0
+const Discord = require('discord.js'); //Version discord.js@11.5.1
+const client = new Discord.Client();
+
+const fetch = require('node-fetch'); //Version node-fetch@2.6.0
 
 const { token } = require('./token');
 
 
-const Discord = require('discord.js'); //Version 11.5.1
-const client = new Discord.Client();
-
-const fetch = require('node-fetch'); //Version 2.6.0
-
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-url = "https://api.nasa.gov/planetary/apod?api_key=79T5Ck3husu3YxYYVeMEqqCMFGp0b77f9Zru8NMw"
-
 function nasapotd(message){
+
+    let url = "https://api.nasa.gov/planetary/apod?api_key=79T5Ck3husu3YxYYVeMEqqCMFGp0b77f9Zru8NMw"
+
+    if(message.content.length >= 6){
+
+    date = message.content.split(" ")[1]
+
+    year = date.split(".")[2]
+    month = date.split(".")[1]
+    day = date.split(".")[0]
+
+    url = url + "&date="+year+"-"+month+"-"+day
+
+    }
+
+    console.log(url)
 
     fetch(url)
     .then(response => {
         return response.json()
     }).then(result => {
-        message.channel.send("**Nasa Picture of the Day**\n"+result.title+"\n"+result.url)
+        embed = {
+            title: result.title,
+            color: 14191372,
+            author: {
+                name: "Nasa Bild vom Tag",
+                icon: message.author.avatarURL               
+            },
+            image: {
+                url: result.url
+                },
+            fields:[
+                { name: 'Explanation', value: result.explanation},
+                { name: 'Link', value: result.url}
+            ]  
+        };
+        message.channel.send({embed})
     })
 
 }
@@ -33,13 +58,12 @@ client.on('message', async message => {
         nasapotd(message)
     }
 
-
 });
 
 
 
 client.on('ready', () => {
-    client.user.setActivity('Nightcore', { type: 'LISTENING' });
+    client.user.setActivity('/help', { type: 'LISTENING' });
   })
 
 client.login(token);
