9ad741e06e8794fcd517b1267fb63d7c9027c8e0
KetasGIT <jan@eisenbach.ch>
2021-11-27 08:19:01 +0100

    small changes

--- bot.js
+++ bot.js
@@ -5,11 +5,6 @@ const fetch = require('node-fetch'); //Version node-fetch@2.6.0
 
 const { token } = require('./token');
 
-
-function truncate(str, n){
-    return (str.length > n) ? str.substr(0, n-1) + " ..."  : str;
-  };
-
 function nasapotd(message){
 
     let url = "https://api.nasa.gov/planetary/apod?api_key=79T5Ck3husu3YxYYVeMEqqCMFGp0b77f9Zru8NMw"
@@ -27,37 +22,41 @@ function nasapotd(message){
     }
 
     fetch(url)
-    .then(response => {
-        return response.json()
-    }).then(result => {
+        .then(response => {
+            return response.json()
+        })
+            .then(result => {
 
-        if(result.code === 400){
-            message.reply("Error\nDatum überprüfen.\nBeispiel: 15.02.2021")
-            return
-        }
+                if(result.code === 400){
+                    message.reply("Error\nDatum überprüfen.\nBeispiel: 15.02.2021")
+                    return
+                }
 
-        description = truncate(result.explanation, 1010)
+                description = truncate(result.explanation, 1010)
 
-        embed = {
-            title: result.title,
-            color: 14191372,
-            author: {
-                name: "Nasa Bild vom Tag",
-                icon: message.author.avatarURL               
-            },
-            image: {
-                url: result.url
-                },
-            fields:[
-                { name: 'Explanation', value: description},
-                { name: 'Link', value: result.url}
-            ]  
-        };
-        message.channel.send({embed})
-    })
+                embed = {
+                    title: result.title,
+                    color: 14191372,
+                    author: {
+                        name: "Nasa Bild vom Tag",
+                        icon: message.author.avatarURL               
+                    },
+                    image: {
+                        url: result.url
+                        },
+                    fields:[
+                        { name: 'Explanation', value: description},
+                        { name: 'Link', value: result.url}
+                    ]  
+                };
+                message.channel.send({embed})
+            })
 
 }
 
+function truncate(str, n){
+    return (str.length > n) ? str.substr(0, n-1) + " ..."  : str;
+  };
 
 client.on('message', async message => {
 
