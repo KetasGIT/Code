5917f93c664bff2e5cb951cc342760421648810d
KetasGIT <jan@eisenbach.ch>
2021-11-27 09:27:14 +0100

    comments for luqman

--- bot.js
+++ bot.js
@@ -7,8 +7,10 @@ const { token } = require('./token');
 
 function nasapotd(message){
 
+    //NASA Api Link
     let url = "https://api.nasa.gov/planetary/apod?api_key=79T5Ck3husu3YxYYVeMEqqCMFGp0b77f9Zru8NMw"
 
+    //custom Date
     if(message.content.length >= 6){
 
         date = message.content.split(" ")[1]
@@ -21,19 +23,25 @@ function nasapotd(message){
 
     }
 
+    //Fetch NASA API JSON
     fetch(url)
         .then(response => {
+            //converts to json
             return response.json()
         })
             .then(result => {
 
+                //if api returns 400, send error message
                 if(result.code === 400){
+                    //replies to message with error code
                     message.reply("Error\nDatum überprüfen.\nBeispiel: 15.02.2021")
                     return
                 }
 
+                //cuts explanation to 1010 characters
                 description = truncate(result.explanation, 1010)
 
+                //Embed builder
                 embed = {
                     title: result.title,
                     color: 14191372,
@@ -54,6 +62,7 @@ function nasapotd(message){
 
 }
 
+//Cuts string to given value n
 function truncate(str, n){
     return (str.length > n) ? str.substr(0, n-1) + " ..."  : str;
   };
@@ -63,10 +72,10 @@ function truncate(str, n){
 
 client.on('message', async message => {
 
-    //return if bot
+    //return if message is from bot
     if (message.author.bot)return;
 
-    //Aufruf Nasa Funktion
+    //Call Nasa Funktion
     if (message.content.startsWith('/nasa')) {
         nasapotd(message)
     }
